7479d536880e236f88479b8123219dd4a2d19e71
AlvaroSRRD <aserranorodera@gmail.com>
2024-02-26 08:51:55 +0100

    Nuevos cambios Alvaro

--- src/polonorte/elfos/ElfoAlvaro.java
+++ src/polonorte/elfos/ElfoAlvaro.java
@@ -1,9 +1,9 @@
 package polonorte.elfos;
 
-public class ElfoAlvaro {
+public class ElfoAlvaro extends ElfoGenerico{
 
     public ElfoAlvaro() {
-            super("Diegu", SeccionLaboralElfo.DISFRACES);
+            super("Alvaro", SeccionLaboralElfo.DISFRACES);
         }
     }
 
